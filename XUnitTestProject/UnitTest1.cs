2d7673cd6862fdb81dc9a0ba6bd3e6481fb45ebf
Sam Perriton-Branch <sam.perriton-branch@vualto.com>
2018-04-16 11:16:29 +0100

    Added failing test for debug.

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -10,5 +10,11 @@ namespace XUnitTestProject
         {
             Assert.True(true);
         }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(false);
+        }
     }
 }
